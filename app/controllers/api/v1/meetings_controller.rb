30d6461737cecc8bf3f2fe8c1c6ac07228b612f6
Samantha Morco <samanthamorco@gmail.com>
2017-06-27 19:42:08 -0400

    add meetings api index json

---
+++ app/controllers/api/v1/meetings_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::MeetingsController < ApplicationController
+  def index
+    @meetings = Meeting.all
+    render :index
+    # render "index.json.jbuilder"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get '/' => 'meetings#index'
-  resources :meetings
+  resources :meetings, except: :destroy
+
+  namespace :api do
+    namespace :v1 do
+      resources :meetings, except: :destroy
+    end
+  end
 end
